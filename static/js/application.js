423e32cf6bb4d2a9905c8a54959efc93ba7597f5
markbarks <marknuttallsmith@gmail.com>
2013-11-25 22:06:36 +0100

    Chrome app working

--- static/js/application.js
+++ static/js/application.js
@@ -1,11 +1,10 @@
 var inbox = new ReconnectingWebSocket("ws://localhost:8000/receive");
 
-
 function addChart(data) {
     var chart;
     nv.addGraph(function () {
-        var width = nv.utils.windowSize().width / 2,
-            height = nv.utils.windowSize().height / 4 - 40;
+        var width = 500,
+            height = 150;
 
         chart = nv.models.multiBarHorizontalChart()
             .width(width)
@@ -28,16 +27,9 @@ function addChart(data) {
         chart.yAxis.tickFormat(d3.format(',.0f'));
         chart.forceY([-800, 800])
 
-
         d3.select('#chart svg')
             .datum(data)
             .call(chart);
-
-        nv.utils.windowResize(chart.update);
-
-        chart.dispatch.on('stateChange', function (e) {
-            nv.log('New State:', JSON.stringify(e));
-        });
 
         return chart;
     });
